b92f39ab80ff952b79de2d3e6a19ecd0a9670eb5
Hiteshmeher <meher.hitesh4@gmail.com>
2022-10-12 11:59:07 +0530

    [Hitesh]: created sample spring app to display Hello from Bridgelabz

---
+++ SpringIntro/src/main/java/com/bridgelabz/com/springintro/SpringIntroApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.com.springintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntroApplication.class, args);
+        System.out.println("hello from Bridgelabz");
+    }
+
+}
